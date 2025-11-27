1f9b0ac2d4889a021ae2b86e47d044a86cd91440
Vitaly Matseiko <realnatisk@gmail.com>
2025-11-27 16:12:00 +0100

    modify specs for create_subscription_service without data_origin

--- spec/services/create_subscription_service_spec.rb
+++ spec/services/create_subscription_service_spec.rb
@@ -5,23 +5,11 @@ require 'rails_helper'
 RSpec.describe CreateSubscriptionService do
   describe '#call' do
     let(:subscription_params) { { id: 'sub_123', customer: 'cus_123' } }
-    let(:mapped_params) { { external_id: 'sub_123', customer_id: 'cus_123' } }  
-    let(:subscription) { instance_double(Subscription) }  
+    subject { described_class.new(subscription_params) }
 
-    context 'stripe origin' do
-      let(:data_origin) { :stripe }
-      let(:stripe_mapper) { instance_double(Stripe::SubscriptionMapper, call: mapped_params) }
-
-      subject { described_class.new(subscription_params, data_origin) }
-
-      before do
-        allow(Stripe::SubscriptionMapper).to receive(:new).with(subscription_params).and_return(stripe_mapper)
-      end
-
-      it 'calls create a subscription' do
-        expect(Subscription).to receive(:create!).with(mapped_params)
-        subject.call
-      end
+    it 'calls create a subscription' do
+      expect(Subscription).to receive(:create!).with(subscription_params)
+      subject.call
     end
   end
 end
