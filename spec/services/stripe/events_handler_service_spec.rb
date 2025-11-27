d26100db3e78393779313953718e94ede1a98262
Vitaly Matseiko <realnatisk@gmail.com>
2025-11-27 16:04:56 +0100

    fix test to make it work for events_handler_service withou data_origin

--- spec/services/stripe/events_handler_service_spec.rb
+++ spec/services/stripe/events_handler_service_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe Stripe::EventsHandlerService do
   describe '#call' do
     let(:event) { double('Stripe::Event') }
     let(:object) { double('EventDataObject') }
-    let(:data_origin) { :stripe }
 
     before do
       allow(event).to receive(:data).and_return(double(object: object))
@@ -15,7 +14,7 @@ RSpec.describe Stripe::EventsHandlerService do
     subject { described_class.new(event) }
 
     context 'when event type is customer.subscription.created' do
-      it 'calls CreateSubscriptionService with the event object and :stripe' do
+      it 'calls CreateSubscriptionService with the event object' do
         allow(event).to receive(:type).and_return('customer.subscription.created')
         expect(CreateSubscriptionService).to receive(:call).with(object, data_origin)
         subject.call
@@ -23,25 +22,26 @@ RSpec.describe Stripe::EventsHandlerService do
     end
 
     context 'when event type is customer.subscription.deleted' do
-      it 'calls CancelSubscriptionService with the event object and :stripe' do
+      it 'calls CancelSubscriptionService with the event object' do
         allow(event).to receive(:type).and_return('customer.subscription.deleted')
-        expect(CancelSubscriptionService).to receive(:call).with(object, data_origin)
+        expect(CancelSubscriptionService).to receive(:call).with(object)
         subject.call
       end
     end
 
     context 'when event type is invoice.paid' do
-      it 'calls CreateInvoiceService with the event object and :stripe' do
+      it 'calls CreateInvoiceService with the event object' do
         allow(event).to receive(:type).and_return('invoice.paid')
-        expect(CreateInvoiceService).to receive(:call).with(object, data_origin)
+        expect(CreateInvoiceService).to receive(:call).with(object)
         subject.call
       end
     end
 
     context 'when event type is unhandled' do
-      it 'prints an unhandled event type message' do
+      it 'logs an info message' do
         allow(event).to receive(:type).and_return('unhandled.event')
-        expect { subject.call }.to output("---- Unhandled event type: unhandled.event ----\n").to_stdout
+        expect(Rails.logger).to receive(:info).with('Unhandled Stripe event type: unhandled.event')
+        subject.call
       end
     end
   end
